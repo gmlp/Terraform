a640166316538fc58406825d4a8bd7a2c45388b4
gmlp <gmlp.24a@gmail.com>
2017-08-07 10:52:58 -0500

    Add aws datasources

--- modules/application/application.tf
+++ modules/application/application.tf
@@ -24,8 +24,14 @@ resource "aws_security_group" "allow_http" {
   }
 }
 
+# data "aws_ami" "app-ami" {
+#     most_recent = true
+#     owners = ["self"]
+# }
+
 resource "aws_instance" "app-server" {
   ami                    = "ami-835b4efa"
+#  ami                    = "${data.aws_ami.app-ami.id}"
   instance_type          = "${lookup(var.instance_type, var.environment)}"
   subnet_id              = "${var.subnet_id}"
   vpc_security_group_ids = ["${distinct(concat(var.extra_sgs, aws_security_group.allow_http.*.id))}"]
--- template.tf
+++ template.tf
@@ -2,8 +2,18 @@ provider "aws" {
   region = "us-west-2"
 }
 
+data "aws_vpc" "management_layer" {
+    id = "vpc-d2f354b4"
+}
+
 resource "aws_vpc" "my_vpc" {
   cidr_block = "${var.vpc_cidr}"
+}
+
+resource "aws_vpc_peering_connection" "my_vpc-management" {
+    peer_vpc_id = "${data.aws_vpc.management_layer.id}"
+    vpc_id = "${aws_vpc.my_vpc.id}"
+    auto_accept = true
 }
 
 resource "aws_subnet" "public" {
@@ -23,6 +33,7 @@ resource "aws_security_group" "default" {
         cidr_blocks = "${var.allow_ssh_access}"
     }
 }
+
 
 module "mighty_trousers" {
   source    = "./modules/application"
